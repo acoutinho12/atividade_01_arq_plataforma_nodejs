c22e5745e1c0bbe79411bd129959e12e9dc272be
André Luis Barbosa Coutinho <andre.coutinho@192.168.1.2>
2022-09-25 13:44:14 -0300

    chore: Adicionando script de start para heroku

--- src/server.js
+++ src/server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const routes = require("./routes");
 
 const app = express();
+const port = process.env.PORT || 3000
 
 app.use(express.json());
 app.use("/api/v1", routes);
 
 async function run() {
 
-  app.listen(3333);
+  app.listen(port);
 }
 
 run().catch(console.error);
